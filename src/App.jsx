242fa6411d8bf5858e4800d437897c4210e68fa3
Jason Smith <jasondsmith2501@gmail.com>
2025-02-05 10:43:50 -0800

    Added some basic CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState } from 'react'
+import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>Hello!</h1>
+      <div>
+        <div class="button-layout">
+          <button>Gimme a card!</button>
+          <button>Shuffle!</button>
+        </div>
+        <div class="cards-layout">
+          <img src="https://images.vat19.com/covers/large/giant-playing-cards.webp" />
+          <img src="https://images.vat19.com/covers/large/giant-playing-cards.webp" />
+          <img src="https://images.vat19.com/covers/large/giant-playing-cards.webp" />
+          <img src="https://images.vat19.com/covers/large/giant-playing-cards.webp" />
+        </div>
+      </div>
     </>
   )
 }
