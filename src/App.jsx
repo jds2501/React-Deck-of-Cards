5df35db2ac7e4475b82c857342afb80947330a12
Jason Smith <jasondsmith2501@gmail.com>
2025-02-05 12:47:07 -0800

    Switched no cards remaining behavior to use a refer and an alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import "./App.css";
 import Action from './Action';
 
 function App() {
   const [deckId, setDeckId] = useState(null);
   const [cardImages, setCardImages] = useState([]);
-  const [giveCardEnabled, setGiveCardEnabled] = useState(false);
+  const noMoreCardsRef = useRef(false);
 
   useEffect(() => {
     async function loadDeck() {
@@ -15,7 +15,6 @@ function App() {
         const deck = await fetch(NEW_DECK);
         const deckJSON = await deck.json();
         setDeckId(deckJSON["deck_id"]);
-        setGiveCardEnabled(true);
       } catch (error) {
         console.error("Failed to load deck", error);
       }
@@ -25,17 +24,21 @@ function App() {
   }, []);
 
   async function drawCard() {
-    const deck = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
-    const drawCardFromDeck = await fetch(deck);
-    const drawCardFromDeckJSON = await drawCardFromDeck.json();
+    if (noMoreCardsRef.current) {
+      alert("Error: no cards remaining!");
+    } else {
+      const deck = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
+      const drawCardFromDeck = await fetch(deck);
+      const drawCardFromDeckJSON = await drawCardFromDeck.json();
 
-    if (drawCardFromDeckJSON["remaining"] === 0) {
-      setGiveCardEnabled(false);
+      if (drawCardFromDeckJSON["remaining"] === 0) {
+        noMoreCardsRef.current = true;
+      }
+
+      setCardImages(cardImages => {
+        return [...cardImages, drawCardFromDeckJSON["cards"][0]["image"]];
+      })
     }
-
-    setCardImages(cardImages => {
-      return [...cardImages, drawCardFromDeckJSON["cards"][0]["image"]];
-    })
   }
 
   async function shuffleDeck() {
@@ -48,7 +51,7 @@ function App() {
     <>
       <div>
         <div className="button-layout">
-          <Action name={"Gimme a card!"} callback={drawCard} enabled={giveCardEnabled} />
+          <Action name={"Gimme a card!"} callback={drawCard} enabled={enabled} />
           <Action name={"Shuffle!"} callback={shuffleDeck} enabled={enabled} />
         </div>
         <div className="cards-layout">
