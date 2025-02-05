0a59227dab82a43e0f863c5f1a97d46af33cf3fc
Jason Smith <jasondsmith2501@gmail.com>
2025-02-05 11:00:03 -0800

    Added deck loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
-import { useState } from 'react'
+import { useEffect, useRef } from 'react'
 import "./App.css";
 
 function App() {
+  const deckIdRef = useRef(null);
+
+  useEffect(() => {
+    async function loadDeck() {
+      const NEW_DECK = "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+
+      try {
+        const deck = await fetch(NEW_DECK);
+        const deckJSON = await deck.json();
+        deckIdRef.current = deckJSON["deck_id"];
+      } catch (error) {
+        console.error("Failed to load deck", error);
+      }
+    }
+
+    loadDeck();
+  }, []);
+
   return (
     <>
       <div>
