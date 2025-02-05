8837ecc5d9557d8a78c70144fde0352f2920f20d
Jason Smith <jasondsmith2501@gmail.com>
2025-02-05 10:44:21 -0800

    Fixed to use class name

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,11 @@ function App() {
   return (
     <>
       <div>
-        <div class="button-layout">
+        <div className="button-layout">
           <button>Gimme a card!</button>
           <button>Shuffle!</button>
         </div>
-        <div class="cards-layout">
+        <div className="cards-layout">
           <img src="https://images.vat19.com/covers/large/giant-playing-cards.webp" />
           <img src="https://images.vat19.com/covers/large/giant-playing-cards.webp" />
           <img src="https://images.vat19.com/covers/large/giant-playing-cards.webp" />
